8eb06e5ac192f0e07cb539bf06619d5abb147ba0
Murali Krishna Tallapudi <muralit@Convergence.com>
2021-09-08 19:17:06 +0530

    log file

--- src/test/java/Test/TestSetUp.java
+++ src/test/java/Test/TestSetUp.java
@@ -6,17 +6,20 @@ import java.io.PrintStream;
 import java.text.SimpleDateFormat;
 
 import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeSuite;
 
 public class TestSetUp extends Base {
 	
-	@BeforeClass
-	public void precleanup() throws IOException, InterruptedException {
+	@BeforeSuite
+	public void CreateLogFile() throws IOException, InterruptedException {
 		NetsfereActivity.killAllBrowserSessions();
 		String udate = new SimpleDateFormat("dd-MM-yy-HHmmss").format(new java.util.Date());
 		if ( Config.getInstance().getExecutionRegion().contains("EU")) {
 			System.setOut(new PrintStream(new FileOutputStream("\\Y:\\NS_Automon_logs\\"+"Monitoring1-" + udate +".txt")));
+//			System.setOut(new PrintStream(new FileOutputStream("C:\\Murali\\NCO-Monit-Logs\\"+"Monitoring1-" + udate +".txt")));
 		} else if ( Config.getInstance().getExecutionRegion().contains("MS2")){
 			System.setOut(new PrintStream(new FileOutputStream("\\Y:\\NS_Automon_logs\\"+"Monitoring2-" + udate +".txt")));
+//			System.setOut(new PrintStream(new FileOutputStream("C:\\Murali\\NCO-Monit-Logs\\"+"Monitoring2-" + udate +".txt")));
 		}
 		System.out.println("Output File Created");
 	}
